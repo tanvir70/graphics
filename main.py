655d97358750dd52d47d31d5bb45376a02e852fd
Tanvir Anjum Rifat <80834028+tanvir70@users.noreply.github.com>
2023-03-04 19:20:18 +0600

    House by rectangle and triangle

--- main.py
+++ main.py
@@ -1,61 +1,37 @@
 from graphics import *
 
+
 def main():
-    a = 3.5
-    b = color_rgb(0,0,0)
+    win = GraphWin("CSE 342", 700, 700)
+    win.setBackground(color_rgb(255, 255, 255))
+    a = 3
+    b = "black"
 
-    win=GraphWin("CSE 342", 700, 700)
-    win.setBackground(color_rgb(255,255,255))
+    tri = Polygon(Point(350, 100), Point(550, 250), Point(150, 250))
+    tri.setWidth(a)
+    tri.setOutline(b)
+    tri.setFill("orange")
+    tri.draw(win)
 
-    img = Image(Point(120, 90), "i.png")
-    img.draw(win)
+    rect = Rectangle(Point(150, 250), Point(550, 600))
 
-    lm = Line(Point(120, 600), Point(550, 600))
-    lm.setWidth(3.5)
-    lm.setOutline(color_rgb(0, 0, 0))
-    lm.draw(win)
+    # set some properties of the rectangle
+    rect.setFill("blue")
+    rect.setOutline(b)
+    rect.setWidth(a)
+    rect.draw(win)
 
-    lm1 = Line(Point(550,600),Point(550,250))
-    lm1.setWidth(a)
-    lm1.setOutline(b)
-    lm1.draw(win)
+    door = Rectangle(Point(300, 450), Point(400, 600))
+    door.setFill("black")
+    door.setOutline(b)
+    door.setWidth(a)
+    door.draw(win)
 
-    lm2 = Line(Point(550,250),Point(120, 250))
-    lm2.setWidth(a)
-    lm2.setOutline(b)
-    lm2.draw(win)
-
-    lm3 = Line(Point(120, 250), Point(120, 600))
-    lm3.setWidth(a)
-    lm3.setOutline(b)
-    lm3.draw(win)
-
-    lm3 = Line(Point(120, 250), Point(335,50))
-    lm3.setWidth(a)
-    lm3.setOutline(b)
-    lm3.draw(win)
-
-    lm3 = Line(Point(335, 50), Point(550,250))
-    lm3.setWidth(a)
-    lm3.setOutline(b)
-    lm3.draw(win)
-
-    door1 = Line(Point(275,600),Point(275,450))
-    door1.setWidth(a)
-    door1.setOutline(b)
-    door1.draw(win)
-
-    door1 = Line(Point(400, 600), Point(400, 450))
-    door1.setWidth(a)
-    door1.setOutline(b)
-    door1.draw(win)
-
-    door1 = Line(Point(275, 450), Point(400, 450))
-    door1.setWidth(a)
-    door1.setOutline(b)
-    door1.draw(win)
-
-
+    window = Rectangle(Point(200, 300), Point(250, 350))
+    window.setFill("black")
+    window.setOutline(b)
+    window.setWidth(a)
+    window.draw(win)
 
     win.getMouse()
     win.close()
